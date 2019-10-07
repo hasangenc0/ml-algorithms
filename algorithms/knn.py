d5cfba4ed836c0991d73b8e80e6c1594ce29855f
Hasan Genç <hasan.genc@trendyol.com>
2019-10-07 17:37:40 +0300

    Add feature knn prediction

--- algorithms/knn.py
+++ algorithms/knn.py
@@ -11,35 +11,68 @@ class KNClassifier:
         self.trainingSet = []
         self.testSet = []
         self.predictions = []
+        self.label = 0
 
-    def loadDataset(self, filename, label):
-        with open(filename, 'rt') as csvfile:
-            lines = csv.reader(csvfile)
+    def load_dataset(self, filename, label, batch=100, titles=False):
+        with open(filename, 'rt') as csv_file:
+            lines = csv.reader(csv_file)
             dataset = list(lines)
-            for c, val in enumerate(dataset):
-                for i, data in enumerate(val):
-                    if i != (label - 1):
-                        dataset[c][i] = float(data)
-            self.dataSet = dataset
+            if titles:
+                del dataset[0]
 
-    def splitTestAndTrainData(self, proportion=0.5):
+            length = int(len(dataset)*batch/100)
+            self.dataSet = [[] for i in range(length)]
+            self.label = label
+            for c, data in enumerate(dataset):
+                if c >= length:
+                    break
+                for i, value in enumerate(data):
+                    if i != self.label-1:
+                        self.dataSet[c].append(self.get_value_for_data(value))
+                    else:
+                        self.dataSet[c].append(value)
+
+    def split_data(self, proportion=0.5):
         for _, data in enumerate(self.dataSet):
             if random.random() < proportion:
                 self.trainingSet.append(data)
             else:
                 self.testSet.append(data)
 
-    def euclideanDistance(self, instance1, instance2, length):
+    def load_data_for_predict(self, filename, titles=False):
+        with open(filename, 'rt') as csv_file:
+            lines = csv.reader(csv_file)
+            dataset = list(lines)
+            if titles:
+                del dataset[0]
+
+            self.testSet = [[] for i in range(len(dataset))]
+            for c, data in enumerate(dataset):
+                for i, value in enumerate(data):
+                    if i != self.label-1:
+                        self.testSet[c].append(self.get_value_for_data(value))
+                    else:
+                        self.testSet[c].append(value)
+
+    @staticmethod
+    def get_value_for_data(value):
+        try:
+            return float(value)
+        except Exception:
+            return float('%d' * len(value) % tuple(map(ord, value)))
+
+    @staticmethod
+    def euclidean_distance(test_instance, training_instance, label):
         distance = 0
-        for i in range(length):
-            distance += pow((instance1[i] - instance2[i]), 2)
+        for i, test in enumerate(test_instance):
+            if i != label-1:
+                distance += pow((test - training_instance[i]), 2)
         return math.sqrt(distance)
 
-    def getNeighbors(self, trainingSet, testInstance, k):
+    def get_neighbors(self, test_instance, training_set, k):
         distances = []
-        length = len(testInstance)-1
-        for _, data in enumerate(trainingSet):
-            dist = self.euclideanDistance(testInstance, data, length)
+        for _, data in enumerate(training_set):
+            dist = self.euclidean_distance(test_instance, data, self.label)
             distances.append((data, dist))
         distances.sort(key=operator.itemgetter(1))
         neighbors = []
@@ -47,15 +80,16 @@ class KNClassifier:
             neighbors.append(distances[i][0])
         return neighbors
 
-    def getResponse(self, neighbors):
-        classVotes = {}
+    @staticmethod
+    def get_response(neighbors):
+        class_votes = {}
         for _, neighbor in enumerate(neighbors):
             response = neighbor[-1]
-            classVotes[response] = classVotes[response] + 1 if response in classVotes else 1
-        sortedVotes = sorted(classVotes.items(), key = operator.itemgetter(1), reverse=True)
-        return sortedVotes[0][0]
+            class_votes[response] = class_votes[response] + 1 if response in class_votes else 1
+        sorted_votes = sorted(class_votes.items(), key=operator.itemgetter(1), reverse=True)
+        return sorted_votes[0][0]
 
-    def getAccuracy(self):
+    def get_accuracy(self):
         correct = 0
         for i in range(len(self.testSet)):
             if self.testSet[i][-1] == self.predictions[i][0]:
@@ -64,20 +98,8 @@ class KNClassifier:
 
     def predict(self, k):
         self.predictions = []
-        for _, data in enumerate(self.testSet):
-            neighbors = knn.getNeighbors(self.trainingSet, data, k)
-            result = knn.getResponse(neighbors)
-            self.predictions.append([result, data[-1]])
+        for i, data in enumerate(self.testSet):
+            neighbors = self.get_neighbors(data, self.trainingSet, k)
+            result = self.get_response(neighbors)
+            self.predictions.append([result, data[self.label-1]])
         return self.predictions
-
-if __name__ == "__main__":
-    knn = KNClassifier()
-    knn.loadDataset('data/iris.csv', label=5)
-    knn.splitTestAndTrainData(proportion=0.66)
-    predictions = knn.predict(3)
-
-    for x in predictions:
-        print('> predicted=' + repr(x[0]), '> actual=' + repr(x[1]))
-
-    accuracy = knn.getAccuracy()
-    print("Accuracy: " + repr(accuracy) + "%")
---
+++ examples.py
@@ -0,0 +1,29 @@
+from algorithms.knn import KNClassifier
+
+def adult_data_knn_accuracy():
+    knn = KNClassifier()
+    knn.load_dataset('data/adult.csv', label=15, batch=1, titles=True)
+
+    # Test prediction accuracy
+    knn.split_data(proportion=0.66)
+    predictions = knn.predict(k=3)
+    for x in predictions:
+        print('> predicted=' + repr(x[0]), '> actual=' + repr(x[1]))
+    accuracy = knn.get_accuracy()
+    print("Accuracy: " + repr(accuracy) + "%")
+
+
+# Predicting data with unknown values
+def adult_data_knn_predict():
+    knn = KNClassifier()
+    knn.load_dataset('data/adult.csv', label=15, batch=1, titles=True)
+
+    knn.load_data_for_predict('data/unpredicted_adults.csv', titles=True)
+    predictions = knn.predict(k=3)
+    for x in predictions:
+        print('> predicted=' + repr(x[0]), '> actual=' + repr(x[1]))
+
+
+if __name__ == "__main__":
+    adult_data_knn_accuracy()
+    adult_data_knn_predict()
